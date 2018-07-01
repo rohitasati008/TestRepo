7c689165e440aa78f1ea0638ac6d3a37545df716
rohitasati008 <rohit.asati@airtel.com>
2018-07-02 01:11:07 +0530

    changes

--- src/com/sapient/Provider.java
+++ src/com/sapient/Provider.java
@@ -8,7 +8,7 @@ import org.w3c.dom.stylesheets.LinkStyle;
 public class Provider {
 void add()
 {
-	System.out.println("hello");
+	System.out.println("hello are ");
 	List<Integer> clist= new ArrayList<>();
 	System.out.println("new changes");
 	 System.out.println("change in feature");
